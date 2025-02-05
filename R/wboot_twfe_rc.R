9150a49213fdb43dd7e9b0f4eec532ffb494e3da
Sarah Baum <122310922+sbaum95@users.noreply.github.com>
2025-02-04 21:38:40 -0500

    Update wboot_twfe_rc.R

--- R/wboot_twfe_rc.R
+++ R/wboot_twfe_rc.R
@@ -13,7 +13,7 @@ wboot_twfe_rc <- function(nn, n, y, dd, post, x, i.weights){
     reg.b <- stats::glm(y ~  dd:post + post + dd + x + offset(n), family = poisson(link = "log))
   } else{
     #reg.b <- stats::lm(y ~  dd:post + post + dd, weights = b.weights)
-    reg.b <- stats::lm(y ~  dd:post + post + dd + offset(n), family = poisson(link = "log))
+    reg.b <- stats::glm(y ~  dd:post + post + dd + offset(n), family = poisson(link = "log))
   }
   twfe.att.b <- reg.b$coefficients["dd:post"]
   #-----------------------------------------------------------------------------
